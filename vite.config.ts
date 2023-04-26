84f9852ab347507d2d7d78ed0343b1c145115ced
schepkej <38034933+jschepke@users.noreply.github.com>
2023-04-26 23:58:32 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: { entry: resolve(__dirname, 'src/index.ts'), name: 'calendar-range' },
+  },
+  plugins: [dts()],
+});
