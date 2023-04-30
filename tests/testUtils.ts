e2c38644671b66569dec8998fbf7b7c7b3be22cb
schepkej <38034933+jschepke@users.noreply.github.com>
2023-04-30 21:19:29 +0200

    test: Add more values to testUtils.ts
    Update the testUtils.ts file to include more values for testing various
    functions.
    
    Add NaN, Infinity, and non empty object to the invalidInputValues array.
    
    Add weekdayTestValues, isNumberTestValues, and isEmptyObjectTestValues
    variables to store arrays of invalid and valid values for testing
    isValidWeekday, isNumber, and isEmptyObject functions respectively.
    
    The purpose of this file and variables is to centralize and organize
    all testing values in one place within the codebase.

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,8 +1,12 @@
+
 export const invalidInputValues = [
 	{ invalidInput: null },
 	{ invalidInput: undefined },
+	{ invalidInput: NaN },
+	{ invalidInput: Infinity },
 	{ invalidInput: [] },
 	{ invalidInput: {} },
+	{ invalidInput: { a: 1, b: "" } },
 	{ invalidInput: 1 },
 	{ invalidInput: 2.5 },
 	{ invalidInput: -1 },
@@ -13,3 +17,69 @@ export const invalidInputValues = [
 	{ invalidInput: true },
 	{ invalidInput: false },
 ];
+
+export const weekdayTestValues = {
+	valid: [1, 2, 3, 4, 5, 6, 7],
+	invalid: [
+		{ invalidInput: null },
+		{ invalidInput: undefined },
+		{ invalidInput: NaN },
+		{ invalidInput: Infinity },
+		{ invalidInput: [] },
+		{ invalidInput: {} },
+		{ invalidInput: { a: 1, b: "" } },
+		// { invalidInput: 1 },
+		{ invalidInput: 2.5 },
+		{ invalidInput: -1 },
+		{ invalidInput: [1] },
+		{ invalidInput: "test" },
+		{ invalidInput: "2021-12-25" },
+		{ invalidInput: ["test"] },
+		{ invalidInput: true },
+		{ invalidInput: false },
+	],
+};
+
+export const isNumberTestValues = {
+	valid: [0, 1, -1, 0.5, -0.5],
+	invalid: [
+		{ invalidInput: null },
+		{ invalidInput: undefined },
+		{ invalidInput: NaN },
+		{ invalidInput: Infinity },
+		{ invalidInput: [] },
+		{ invalidInput: {} },
+		{ invalidInput: { a: 1, b: "" } },
+		// { invalidInput: 1 },
+		// { invalidInput: 2.5 },
+		// { invalidInput: -1 },
+		{ invalidInput: [1] },
+		{ invalidInput: "test" },
+		{ invalidInput: "2021-12-25" },
+		{ invalidInput: ["test"] },
+		{ invalidInput: true },
+		{ invalidInput: false },
+	],
+};
+
+export const isEmptyObjectTestValues = {
+	valid: [],
+	invalid: [
+		{ invalidInput: null },
+		{ invalidInput: undefined },
+		{ invalidInput: NaN },
+		{ invalidInput: Infinity },
+		{ invalidInput: [] },
+		// { invalidInput: {} },
+		{ invalidInput: { a: 1, b: "" } },
+		{ invalidInput: 1 },
+		{ invalidInput: 2.5 },
+		{ invalidInput: -1 },
+		{ invalidInput: [1] },
+		{ invalidInput: "test" },
+		{ invalidInput: "2021-12-25" },
+		{ invalidInput: ["test"] },
+		{ invalidInput: true },
+		{ invalidInput: false },
+	],
+};
