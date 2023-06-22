0b0f51fd2b09c91ed585eb7c7a53e8b132d48093
schepkej <38034933+jschepke@users.noreply.github.com>
2023-06-22 23:49:57 +0200

    refactor(validators): remove unused files and code
    Simplify validation by removing redundant functions after refactoring
    methods.

--- src/validators/index.ts
+++
@@ -1,3 +0,0 @@
-export { validateRangeOptions } from "./validateRangeOptions";
-export { validateExtendRangeOptions } from "./validateExtendRangeOptions";
-export { validateRangeOptionsMonthExact } from "./validateRangeOptionsMonthExact";
