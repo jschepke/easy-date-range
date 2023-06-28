426d21824d2ebd1b348fa7c6347d02ac73a36142
schepkej <38034933+jschepke@users.noreply.github.com>
2023-06-28 20:32:38 +0200

    chore: update vite config
    Specify luxon as external library to not be included in the bundle.
    
    Change default coverage folder location.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
@@ -5,6 +7,19 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
 	build: {
 		lib: { entry: resolve(__dirname, "src/index.ts"), name: "easy-date-range" },
+		rollupOptions: {
+			external: ["luxon"],
+			output: {
+				globals: {
+					luxon: "DateTime",
+				},
+			},
+		},
 	},
 	plugins: [dts()],
+	test: {
+		coverage: {
+			reportsDirectory: "tests/coverage",
+		},
+	},
 });
