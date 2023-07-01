558eebc534d5ccf00b170816a0d9d86f996d23b3
schepkej <38034933+jschepke@users.noreply.github.com>
2023-07-01 22:30:01 +0200

    test(utils): add test for validateRangeType

--- tests/validators/common.test.ts
+++ tests/validators/common.test.ts
@@ -1,8 +1,35 @@
-import { validateObjectArgument } from "../../src/validators/common";
+import { RANGE_TYPE } from "../../src/constants";
+import {
+	validateObjectArgument,
+	validateRangeType,
+} from "../../src/validators/common";
 import { TestValues } from "../testUtils";
 import { describe, expect, test } from "vitest";
 
 describe("Common validators", () => {
+	describe("validateRangeType", () => {
+		describe("Given non valid argument", () => {
+			test.each(new TestValues().excludeByName(["string 'test'"]))(
+				"throws an error for argument: $name",
+				({ value }) => {
+					expect(() => validateRangeType(value)).toThrowError();
+				},
+			);
+		});
+
+		describe("Given valid argument", () => {
+			const rangeTypes = Object.values(RANGE_TYPE);
+			console.log(rangeTypes);
+
+			test.each(rangeTypes)(
+				"doesn't return anything for valid rangeType: %s",
+				(rangeType) => {
+					expect(validateRangeType(rangeType)).toBeUndefined();
+				},
+			);
+		});
+	});
+
 	describe("validateObjectArgument", () => {
 		describe("Given non valid argument", () => {
 			test.each(new TestValues().excludeByName(["object { a: 1, b: 'foo' }"]))(
