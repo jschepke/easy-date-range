84a155ac8d4ff3502c4bb1d5f48f0678c8bbf2db
schepkej <38034933+jschepke@users.noreply.github.com>
2023-07-05 18:45:30 +0200

    feat: add emptyDir function to vite config
    Add emptyDir function to vite config to delete all files and
    subdirectories in the dist directory before build run. This ensures that
    the dist directory is always clean and up-to-date.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 /// <reference types="vitest" />
 
+import { existsSync, lstatSync, readdirSync, rmdirSync, unlinkSync } from "fs";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
+emptyDir(resolve(__dirname, "dist"));
+
 export default defineConfig({
 	build: {
-		lib: { entry: resolve(__dirname, "src/index.ts"), name: "easy-date-range" },
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			fileName: "easy-date-range",
+			name: "easy-date-range",
+		},
 		rollupOptions: {
 			external: ["luxon"],
-			output: {
-				globals: {
-					luxon: "DateTime",
-				},
-			},
 		},
 	},
 	plugins: [dts()],
@@ -23,3 +25,21 @@ export default defineConfig({
 		},
 	},
 });
+
+function emptyDir(dir: string): void {
+	if (!existsSync(dir)) {
+		return;
+	}
+
+	for (const file of readdirSync(dir)) {
+		const abs = resolve(dir, file);
+
+		// baseline is Node 12 so can't use rmSync
+		if (lstatSync(abs).isDirectory()) {
+			emptyDir(abs);
+			rmdirSync(abs);
+		} else {
+			unlinkSync(abs);
+		}
+	}
+}
